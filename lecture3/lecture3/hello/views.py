9fa63c486a6b8f86c1a6529bd858eb151a99c484
TyPage <40603745+TyPage@users.noreply.github.com>
2024-10-10 01:14:32 +0000

    django test

---
+++ lecture3/lecture3/hello/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request, "hello/index.html")
+
+def tyler(request):
+    return HttpResponse("Hello Tyler!")
+
+def greet(request, name):
+    return HttpResponse("Hello, {name}!")
